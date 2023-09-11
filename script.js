c52376e34646faf56cc53c14a1a04f33dc6d02d7
Deshire Nuñez <rivas.deshire@gmail.com>
2023-09-11 11:30:11 -0500

    change menu

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const menuToggle = document.querySelector('.toggle');
+
+const navigation = document.querySelector('.navigation');
+
+menuToggle.onclick=function(){
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
